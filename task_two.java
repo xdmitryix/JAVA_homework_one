68d7f33503e4f5e9348a56f7476f1041ee4d6ba3
dmitry <rbdeman@yandex.ru>
2023-03-02 23:46:25 +0300

    сделал вторую задачу

--- task_two.java
+++ task_two.java
@@ -1,3 +1,19 @@
+// Вывести все простые числа от 1 до 1000
+
+
 public class task_two {
-    
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                } 
+            }
+            if ((i % 1 == 0) && (i % i == 0) && (count == 2)) {
+                System.out.println(i);
+            }
+        }
+    }
 }
